97f01e3af9978211f948e9d723d7ef88239987db
neuen <kansapat4921@gmail.com>
2023-09-09 03:32:16 +0700

    Added request, and added basic training model

--- src/dataPrep.py
+++ src/dataPrep.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
 df = pd.DataFrame({
-    'user_id': [],
-    'reputation': [],
-    'question': [],
-    'answer': [],
-    'upvote': [],
-    'downvote': []
+    'user_id': [1],
+    'reputation': [1],
+    'question': [1],
+    'answer': [1],
+    'upvote': [1],
+    'downvote': [1]
 })
 
-df = df.dropna(inplace=True)
+df.dropna(inplace=True)
 df['interaction'] = df['question'] + df['answer']
 df['effectiveness'] = df['upvote'] / (df['downvote'] + df['upvote'])
 
