ef7c351550f45bb659a1fc6fc9376fa7d8963827
neuen <kansapat4921@gmail.com>
2023-09-09 05:55:47 +0700

    Added more robust and complete data fetching

--- src/fetchUserData.py
+++ src/fetchUserData.py
@@ -2,11 +2,72 @@ import requests
 import json
 from dotenv import load_dotenv
 import os
+import time
+import pandas as pd
 
 load_dotenv()
 
-def fetchUserData(username):
-    url = os.getenv("API_KEY")
-    response = requests.get(url)
-    data = json.loads(response.text)
-    return data
+api_key = os.getenv('API_KEY')
+base_url = 'https://api.stackexchange.com/2.3/users/'
+
+def fetchUserID(page=1, sort='creation', order='desc'):
+    try:
+        params = {
+        'page': page,
+        'pagesize': 100,
+        'sort': sort,
+        'order': order,
+        'site': 'stackoverflow',
+        'key': api_key
+    }
+        response = requests.get(base_url, params=params)
+        data = json.loads(response.text)
+        user_ids = [user['user_id'] for user in data.get('items', [])]
+        time.sleep(1 / 30)
+        return user_ids
+    
+    except Exception as e:
+        print(e)
+        return []
+    
+def fetchUserData(user_ids):
+    user_data_list = []
+    
+    for user_id in user_ids:
+        user_data = {}
+        
+        try:
+            # Fetch basic user information
+            response = requests.get(f"{base_url}{user_id}?site=stackoverflow&key={api_key}")
+            basic_data = json.loads(response.text)
+            user_data['basic'] = basic_data.get('items', [])[0] if 'items' in basic_data else {}
+            time.sleep(1 / 30)
+            
+            # Fetch user's questions
+            response = requests.get(f"{base_url}{user_id}/posts?site=stackoverflow&key={api_key}")
+            posts_data = json.loads(response.text)
+            user_data['posts'] = posts_data.get('items', [])
+            time.sleep(1 / 30)
+
+            # Fetch tags user are active in
+            response = requests.get(f"{base_url}{user_id}/tags?site=stackoverflow&key={api_key}")
+            tags_data = json.loads(response.text)
+            user_data['tags'] = tags_data.get('items', [])
+            time.sleep(1 / 30)
+
+            # Fetch user's reputation
+            response = requests.get(f"{base_url}{user_id}/reputation?site=stackoverflow&key={api_key}")
+            reputation_data = json.loads(response.text)
+            user_data['reputation'] = reputation_data.get('items', [])
+            time.sleep(1 / 30)
+        
+        except Exception as e:
+            print(e, f"error for user_id: {user_id}") 
+            continue
+
+        user_data_list.append(user_data)
+
+    df = pd.DataFrame(user_data_list)
+    return df
+
+print(fetchUserData(fetchUserID()))
---
+++ src/main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LinearRegression
+from fetchUserData import fetchUserData
+
